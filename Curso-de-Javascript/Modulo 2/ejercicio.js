79b1958c65e76a2f3be607a9613f03314ee1468b
Diego <87611871+zaelme@users.noreply.github.com>
2023-01-22 07:53:32 +0100

    Create ejercicio.js

---
+++ Curso-de-Javascript/Modulo 2/ejercicio.js
@@ -0,0 +1,16 @@
+const MiNombre = "Diego";
+const MiEdad  = 24;
+let SoyDesarrolador = true;
+const fecha_nacimiento = new Date("August 10 1998")
+const libro = {
+    titulo: "Guia del autoestopista galactico",
+    autor: "Douglas Adams",
+    fecha: "12 de Octubre, 1979 " ,
+    url: "https://es.wikipedia.org/wiki/Gu%C3%ADa_del_autoestopista_gal%C3%A1ctico_(libro)",
+}
+
+
+
+const Lista1 = [MiNombre, MiEdad, SoyDesarrolador, fecha_nacimiento, libro]
+
+console.log(Lista1)
